97c10b7d007f27524f5944070fa1ec3899cbc973
AhmetErenLaçinbala <ahmetlacinbala@gmail.com>
2022-12-13 05:29:03 +0300

    .

--- src/comps/comp20/comp20.js
+++ src/comps/comp20/comp20.js
@@ -8,29 +8,8 @@ export default function Comp17(props) {
 
         <div className="comp compFullSize" style={{ backgroundColor: "rgba(11,11,11,0.5)" }}>
             <div className="openSansFont fontW700 compContent bigprocon ">
-                <div>
-                    <p className='fontS48'>Pam’ın Avantajları</p>
-                    <p className='fontS40 fontW400'>● PAM, daha basit.</p>
-                    <p className='fontS40 fontW400'>● Daha az karmaşık bir süreçtir.</p>
-                    <p className='fontS40 fontW400'>● PAM'ın verici ve alıcılarının tasarımı
-                        oldukça basit bir iştir.</p>
-                    <p className='fontS40 fontW400'>● Diğer tasarımlardan daha az karmaşıktır.</p>
-                    <p className='fontS40 fontW400'>● Darbe Genlik Modülasyonu, diğer darbe
-                        modülasyon sinyallerini üretebilir.</p>
-                    <p className='fontS40 fontW400'>● Mesaj sinyalini o anda taşıyabilir.</p>
-                </div>
-                <div>
-                    <p className='fontS48'>Pam’ın Dezavantajları</p>
-                    <p className='fontS40 fontW400'>● PAM için, iletim için bant genişliği daha
-                        büyük olmalıdır.</p>
-                    <p className='fontS40 fontW400'>● PAM’da büyük bir gürültü sorunu vardır.</p>
-                    <p className='fontS40 fontW400'>● PAM sinyali değişir, böylece iletim için
-                        gerekli olan güç artabilir.</p>
-
-                </div>
-                <div>
-
-                </div>
+                <div><img src="" alt="" /></div>
+                <div><img src="" alt="" /></div>
 
             </div>
         </div>
